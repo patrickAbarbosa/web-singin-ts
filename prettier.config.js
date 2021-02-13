f7f8be5d06dde6405392f160351415b777d14f55
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-02-12 22:40:17 -0300

    🔧 Configure Project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf'
+}
