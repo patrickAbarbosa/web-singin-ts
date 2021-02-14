2167d0fee5664df4e1256fca7d98a4b11d6d6f63
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-02-14 11:18:14 -0300

    ✨ Create AppButton component

---
+++ src/modules/Common/components/AppButton/AppButton.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react'
+import { ButtonProps } from '@chakra-ui/react'
+
+import { AppButtonStyled } from './style'
+
+const AppButtonVariantStyle = {
+  solid: {
+    background: 'linear-gradient(267.79deg, #383E71 0%, #9D25B0 99.18%)',
+    boxShadow: '0px 10px 25px #CF99DB',
+    color: 'white',
+    _focus: {
+      opacity: 0.8,
+    },
+    _hover: {
+      opacity: 0.8,
+    },
+  },
+}
+
+export const AppButton: React.FC<ButtonProps> = ({ children, variant = 'solid', ...buttonProps }) => {
+  const variantStyle = useMemo(() => AppButtonVariantStyle[variant], [variant])
+
+  return (
+    <AppButtonStyled {...variantStyle} {...buttonProps}>
+      {children}
+    </AppButtonStyled>
+  )
+}
