9ef3da80e181f3b900da13dbb98159f30bc08e8c
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-02-14 18:55:00 -0300

    ✨ Create useSignIn hook

---
+++ src/modules/pages/SignIn/hooks/useSignin.tsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchSignIn } from 'src/store/actions/signin'
+import { IUser } from 'src/store/reducers/user'
+
+interface HooKReturn {
+  signIn(email: string, password: string): Promise<unknown>
+  isAuthenticate: boolean
+  isLoading: boolean
+  error: unknown
+}
+
+export const useSignin = (): HooKReturn => {
+  const dispatch = useDispatch()
+  const { loading: isLoading, error, authorizationToken } = useSelector(
+    (reducer: { userReducer: IUser }) => reducer.userReducer,
+  )
+
+  const signIn = async (email: string, password: string) => {
+    dispatch(fetchSignIn({ email, password }))
+  }
+
+  return {
+    signIn,
+    isAuthenticate: authorizationToken !== '',
+    isLoading,
+    error,
+  }
+}
